a633e718697a955f836e8af77e34b2419446444c
Eunique-star <euniceohilebo05@gmail.com>
2025-07-23 22:56:17 +0100

    Added practicalClass file

---
+++ practicalClass.py
@@ -0,0 +1,13 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+sum = num1 + num2
+difference = num1 - num2
+multiply = num1 * num2
+division = num1 / num2
+
+print(f"These are the results:")
+print(f"Sum: {sum}")
+print(f"Subtraction: {difference}")
+print(f"Multiplication: {multiply}")
+print(f"Division: {division}")
